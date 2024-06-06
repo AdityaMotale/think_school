3e248519cb5b114052d6a6cae3ac9d85192146ef
Adii <77727680+AdityaMotale@users.noreply.github.com>
2024-06-07 00:08:18 +0530

    completed appBar and bottomNavBar

--- lib/views/home.view.dart
+++ lib/views/home.view.dart
@@ -1,18 +1,123 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class HomeView extends StatelessWidget {
+final _bottomNavBarItems = [
+  "home_48.svg",
+  "play_48.svg",
+  "books_48.svg",
+  "read_48.svg"
+];
+
+class HomeView extends StatefulWidget {
   const HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int _selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Text("Home"),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          toolbarHeight: MediaQuery.of(context).size.height * 0.09,
+          title: const Text(
+            "ThinkSchool",
+            style: TextStyle(
+              fontFamily: "Dm Serif Display",
+            ),
           ),
-        ],
+          actions: [
+            Container(
+              width: 32,
+              height: 32,
+              decoration: const BoxDecoration(
+                color: Color(0xFFF3EDFC),
+                shape: BoxShape.circle,
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: SvgPicture.network(
+                  "https://adityamotale.github.io/think_school_release/public/icons/search_32.svg",
+                ),
+              ),
+            ),
+            const SizedBox(width: 20),
+            Container(
+              width: 32,
+              height: 32,
+              decoration: const BoxDecoration(
+                color: Color(0xFFF3EDFC),
+                shape: BoxShape.circle,
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(100),
+                child: SvgPicture.network(
+                  "https://adityamotale.github.io/think_school_release/public/icons/profile_32.svg",
+                ),
+              ),
+            ),
+            const SizedBox(width: 20),
+          ],
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(2),
+            child: Container(
+              color: const Color(0xFFE2DEFD),
+              height: 2,
+            ),
+          ),
+        ),
+        body: const Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Center(
+              child: Text("Home"),
+            ),
+          ],
+        ),
+        bottomNavigationBar: Container(
+          decoration: const BoxDecoration(
+            color: Colors.white,
+            border: Border(
+              top: BorderSide(
+                color: Color(0xFFE2DEFD),
+                width: 2,
+              ),
+            ),
+          ),
+          height: MediaQuery.of(context).size.height * 0.09,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              for (int i = 0; i < _bottomNavBarItems.length; i++)
+                InkWell(
+                  onTap: () {
+                    _selectedIndex = i;
+                    setState(() {});
+                  },
+                  child: Container(
+                    width: 48,
+                    height: 48,
+                    decoration: BoxDecoration(
+                      color: i == _selectedIndex
+                          ? const Color(0xFF8B93FF)
+                          : const Color(0xFFF3EDFC),
+                      shape: BoxShape.circle,
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: SvgPicture.network(
+                        "https://adityamotale.github.io/think_school_release/public/icons/${_bottomNavBarItems[i]}",
+                      ),
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
       ),
     );
   }
